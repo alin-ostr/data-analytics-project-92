be9759d8da787f39c49600de38a25f3889012084
alin-ostr <alinostr17@gmail.com>
2025-10-29 19:46:05 +0300

    Create age_groups.sql

---
+++ age_groups.sql
@@ -0,0 +1,23 @@
+-- Отчёт по количеству покупателей в разных возрастных группах.
+WITH age_summary AS (
+    SELECT
+        CASE
+            WHEN age BETWEEN 16 AND 25 THEN '16-25'
+            WHEN age BETWEEN 26 AND 40 THEN '26-40'
+            ELSE '40+'
+        END AS age_category,
+        COUNT(customer_id) AS age_count
+    FROM customers
+    WHERE age >= 16
+    GROUP BY
+        CASE
+            WHEN age BETWEEN 16 AND 25 THEN '16-25'
+            WHEN age BETWEEN 26 AND 40 THEN '26-40'
+            ELSE '40+'
+        END
+)
+SELECT
+    age_category,
+    age_count
+FROM age_summary
+ORDER BY age_category;
