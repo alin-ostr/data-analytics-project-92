11896733e6dfea1b2a3994cfbd9c60a5d3134be8
alin-ostr <alinostr17@gmail.com>
2025-11-07 16:54:44 +0300

    lowest_average_income.sql

--- lowest_average_income.sql
+++ lowest_average_income.sql
@@ -1,31 +1,32 @@
 -- Формирование отчета о выявлении продавцов с выручкой ниже средней.
 WITH
-  each_avg AS (
+each_avg AS (
     SELECT
-      e.first_name,
-      e.last_name,
-      FLOOR(SUM(s.quantity * p.price) / COUNT(*)) AS average_income -- средняя выручка по каждому продавцу
+        e.first_name,
+        e.last_name,
+        FLOOR(SUM(s.quantity * p.price) / COUNT(*)) AS average_income
     FROM
-      employees e
-      INNER JOIN sales s ON s.sales_person_id = e.employee_id
-      INNER JOIN products p ON p.product_id = s.product_id
+        employees AS e
+    INNER JOIN sales AS s ON e.employee_id = s.sales_person_id
+    INNER JOIN products AS p ON s.product_id = p.product_id
     GROUP BY
-      e.first_name,
-      e.last_name
-  ),
-  total_avg AS (
-    SELECT
-      AVG(average_income) AS t_avg -- общая стредняя выручка
+        e.first_name,
+        e.last_name
+),
+
+total_avg AS (
+    SELECT AVG(average_income) AS t_avg
     FROM
-      each_avg
-  )
+        each_avg
+)
+
 SELECT
-  CONCAT(first_name, ' ', last_name) AS seller,
-  average_income
+    average_income,
+    CONCAT(first_name, ' ', last_name) AS seller
 FROM
-  each_avg
-  CROSS JOIN total_avg -- сравнение значений
+    each_avg
+CROSS JOIN total_avg
 WHERE
-  t_avg > average_income
+    t_avg > average_income
 ORDER BY
-  average_income ASC;
+    average_income ASC;
