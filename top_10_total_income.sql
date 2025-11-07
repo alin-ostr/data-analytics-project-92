d3470e24d1e78e07ce3f4e7897b9ba008894acc0
alin-ostr <alinostr17@gmail.com>
2025-11-07 16:49:59 +0300

    top_10_total_income.sql

--- top_10_total_income.sql
+++ top_10_total_income.sql
@@ -1,15 +1,15 @@
 -- Отчёт об определении десяти продавцов с наибольшей выручкой.
-SELECT 
-  CONCAT(e.first_name, ' ', e.last_name) AS seller,
-  COUNT(s.sales_person_id) AS operations,
-  FLOOR(SUM(s.quantity * p.price)) AS income
+SELECT
+    CONCAT(e.first_name, ' ', e.last_name) AS seller,
+    COUNT(s.sales_person_id) AS operations,
+    FLOOR(SUM(s.quantity * p.price)) AS income
 FROM
-  employees e
-  INNER JOIN sales s ON s.sales_person_id = e.employee_id
-  INNER JOIN products p ON p.product_id = s.product_id
+    employees AS e
+INNER JOIN sales AS s ON e.employee_id = s.sales_person_id
+INNER JOIN products AS p ON s.product_id = p.product_id
 GROUP BY
-  CONCAT(e.first_name, ' ', e.last_name)
+    CONCAT(e.first_name, ' ', e.last_name)
 ORDER BY
-  income DESC
+    income DESC
 LIMIT
-  10;
+    10;
